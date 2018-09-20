f4b09e266e0417bfc98123ddfe98d1aa36a96b10
Jessie <jesse.reyna01@utrgv.edu>
2018-09-20 08:23:01 -0500

    Added Singleton with requestqueue

--- app/src/main/java/com/example/jessi/rjt_project_01/MySingleton.java
+++ app/src/main/java/com/example/jessi/rjt_project_01/MySingleton.java
@@ -1,6 +1,8 @@
 package com.example.jessi.rjt_project_01;
 
 import android.content.Context;
+
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
@@ -10,6 +12,12 @@ public class MySingleton {
     private RequestQueue requestQueue;
     private static Context mCtx;
 
+    private MySingleton(Context context)
+    {
+        mCtx = context;
+        requestQueue = getRequestQueue();
+    }
+
     public RequestQueue getRequestQueue()
     {
         if(requestQueue == null)
@@ -17,5 +25,20 @@ public class MySingleton {
             requestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
         }
         return requestQueue;
+    }
+
+    public static synchronized MySingleton getInstance(Context context)
+    {
+        if (mInstance == null)
+        {
+            mInstance = new MySingleton(context);
+
+        }
+        return mInstance;
+    }
+
+    public<T> void addToRequestQueue(Request<T> request)
+    {
+        requestQueue.add(request);
     }
 }
